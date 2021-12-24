888c47f588c837bb7a9ef4dc82aa229d14b9978f
Anton Turchenko <antont@default-value.com>
2021-12-24 16:02:13 +0200

    Fixed base entities

--- src/main/java/com/antont/petclinic/v2/db/entity/base/IdEntity.java
+++ src/main/java/com/antont/petclinic/v2/db/entity/base/IdEntity.java
@@ -7,7 +7,7 @@ import java.math.BigInteger;
 public class IdEntity {
     @Id
     @Column(name = "Id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
 
     public BigInteger getId() {
--- src/main/java/com/antont/petclinic/v2/db/entity/base/NameEntity.java
+++ src/main/java/com/antont/petclinic/v2/db/entity/base/NameEntity.java
@@ -1,21 +1,20 @@
 package com.antont.petclinic.v2.db.entity.base;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class NameEntity extends IdEntity{
 
     @Column(name = "Name")
-    private String Name;
+    private String name;
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 }
 
